fd51874a690bf72235650cb58958988ab3e1d8be
RoySamson-stack <rsam9009@gmail.com>
2021-12-01 17:09:35 +0300

    adding the remove, add save and the main function to the program

--- main.py
+++ main.py
@@ -1,3 +1,42 @@
 import json
 import os.path
 
+class Inventory:
+    pets = {}
+
+
+    def __init__(self):
+        self.load()
+
+
+    def add(self, key, qty): 
+      q = 0
+      if key in self.pets:
+        v = self.pets[key]
+        q = v + qty
+      else:
+         q = qty
+         self.pets[key] = q 
+         print(f'Added {qty} {key}: total = {self.pts[key]}')
+
+      
+    def remove(self, key, qty):
+      q = 0
+      if key in self.pets:
+        v = self.pets[key]
+        q = v - qty
+
+      self.pets[key] = q 
+      print(f'Removed {qty} {key}: total = {self.pts[key]}')
+
+
+
+    def display(self, key, qty):
+      for key, value  in self.pets.items():
+        print(f'{key}: {value}') 
+
+    def save(self):
+      pass  
+
+    def load(self):
+      pass  
